ffe699b2434315d200892660c63a04415b2f8b32
Serkan Aysan <serkanaysann@gmail.com>
2017-10-20 16:04:56 +0300

    fox-api-test

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+//Initialize routes
+app.use('/api', require('./routes/api'));
+
+
+app.get('/',function(req, res){
+	console.log('get request');
+	res.end();
+});
+
+
+
+app.listen(process.env.port || 8080, function(){
+	console.log('now listening for request');
+});
+
+/*meantraDB
+meantra_root
+y83SmUY0*/
